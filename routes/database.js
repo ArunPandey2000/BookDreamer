84f0a1d4a35c3d09b940e0a1117f37057be46f98
LauraXiulan <laurakramer91@gmail.com>
2016-12-07 16:17:06 +0100

    Get right values with less code, only have to fix the genres

--- routes/database.js
+++ routes/database.js
@@ -19,7 +19,10 @@ db.book = db.conn.define('book', {
 	author: sequelize.STRING,
 	genre: sequelize.ARRAY(sequelize.STRING),
 	pages: sequelize.INTEGER,
-	linkid: sequelize.INTEGER
+	linkid: sequelize.INTEGER,
+	series: sequelize.BOOLEAN,
+	published: sequelize.INTEGER,
+	language: sequelize.STRING
 })
 
 
@@ -29,165 +32,244 @@ db.book = db.conn.define('book', {
 // Create test User
 db.conn.sync({force: true}).then( database => {
 	db.book.create({
-		title: "Harry Potter and the Sorcerer's Stone",
+		title: "Harry Potter and the Sorcerer's Stone(Harry Potter #1)",
 		author: "J.K. Rowling",
 		genre: ["Fantasy", "Young Adult", "Fiction"],
 		pages: 320,
-		linkid: 3
+		linkid: 3,
+		series: true,
+		published: 1997,
+		language: "English"
 	})
 	db.book.create({
 		title: "The Hitchhiker's Guide to the Galaxy",
 		author: "Adam Douglas",
 		genre: ["Science Fiction", "Humor", "Fiction"],
 		pages: 216,
-		linkid: 11
+		linkid: 11,
+		series: true,
+		published: 1995,
+		language: "English"
 	})
 	db.book.create({
 		title: "Mansfield Park",
 		author: "Jane Austen",
 		genre: ["Classics", "Romance", "Fiction"],
 		pages: 560,
-		linkid: 45032
+		linkid: 45032,
+		series: false,
+		published: 2003,
+		language: "English"
 	})
 	db.book.create({
 		title: "The Magician's Guild(The Black Magician Trilogy #1)",
 		author: "Trudi Canavan",
 		genre: ["Fantasy", "Young Adult", "Fiction"],
 		pages: 467,
-		linkid: 28249
+		linkid: 28249,
+		series: true,
+		published: 2004,
+		language: "English"
 	})
 	db.book.create({
 		title: "Murder at the Vicarage(Miss Marple #1)",
 		author: "Agatha Christie",
 		genre: ["Mystery", "Classics", "Fiction"],
 		pages: 288,
-		linkid: 16331
+		linkid: 16331,
+		series: true,
+		published: 2006,
+		language: "English"
 	})
 	db.book.create({
 		title: "A Tale of Two Cities",
 		author: "Charles Dickens",
 		genre: ["Classics", "Literature", "Fiction"],
 		pages: 489,
-		linkid: 1953
+		linkid: 1953,
+		series: false,
+		published: 2003,
+		language: "English"
 	})
 	db.book.create({
 		title: "Gone Girl",
 		author: "Gillian Flynn",
 		genre: ["Mystery", "Thriller", "Fiction"],
 		pages: 555,
-		linkid: 19288043
+		linkid: 19288043,
+		series: false,
+		published: 2014,
+		language: "English"
 	})
 	db.book.create({
-		title: "Outlander",
+		title: "Outlander(Outlander #1)",
 		author: "Diana Gabaldon",
 		genre: ["Romance", "Historical Fiction", "Fantasy"],
 		pages: 896,
-		linkid: 10964
+		linkid: 10964,
+		series: true,
+		published: 2005,
+		language: "English"
 	})
 	db.book.create({
 		title: "The Silence of the Lambs(Hannibal Lecter #2)",
 		author: "Thomas Harris",
 		genre: ["Horror", "Thriller", "Fiction"],
 		pages: 338,
-		linkid: 23807
+		linkid: 23807,
+		series: true,
+		published: 2002,
+		language: "English"
 	})
 	db.book.create({
 		title: "The Kite Runner",
 		author: "Khaled Hosseini",
 		genre: ["Contemporary", "Historical Fiction", "Fiction"],
 		pages: 371,
-		linkid: 77203
+		linkid: 77203,
+		series: false,
+		published: 2004,
+		language: "English"
 	})
 	db.book.create({
 		title: "Les Miserables",
 		author: "Victor Hugo",
 		genre: ["Classics", "Literature", "Fiction"],
 		pages: 1463,
-		linkid: 24280
+		linkid: 24280,
+		series: false,
+		published: 1987,
+		language: "English"
 	})
 	db.book.create({
 		title: "It",
 		author: "Stephen King",
 		genre: ["Horror", "Fantasy", "Fiction"],
 		pages: 1116,
-		linkid: 830502
+		linkid: 830502,
+		series: false,
+		published: 1987,
+		language: "English"
 	})
 	db.book.create({
 		title: "Air Awakens(Air Awakens #1)",
 		author: "Elisa Kova",
 		genre: ["Fantasy", "Young Adult", "Magic"],
 		pages: 377,
-		linkid: 23127048
+		linkid: 23127048,
+		series: true,
+		published: 2015,
+		language: "English"
 	})
 	db.book.create({
 		title: "Prince of Thorns(The Broken Empire #1)",
 		author: "Mark Lawrence",
 		genre: ["Fantasy", "Dark Fantasy", "Fiction"],
 		pages: 384,
-		linkid: 9579634
+		linkid: 9579634,
+		series: true,
+		published: 2011,
+		language: "English"
 	})
 	db.book.create({
 		title: "To Kill a Mockingbird(To Kill a Mockingbird #1)",
 		author: "Harper Lee",
 		genre: ["Classics", "Historical Fiction", "Fiction"],
 		pages: 324,
-		linkid: 2657
+		linkid: 2657,
+		series: true,
+		published: 2006,
+		language: "English"
 	})
 	db.book.create({
 		title: "I Am Number Four(Lorien Legacies #1)",
 		author: "Pittacus Lore",
 		genre: ["Fantasy", "Young Adult", "Science Fiction"],
 		pages: 452,
-		linkid: 7747374
+		linkid: 7747374,
+		series: true,
+		published: 2010,
+		language: "English"
 	})
 	db.book.create({
 		title: "Life of Pi",
 		author: "Yann Martel",
 		genre: ["Fantasy", "Adventure", "Fiction"],
 		pages: 460,
-		linkid: 4214
+		linkid: 4214,
+		series: false,
+		published: 2006,
+		language: "English"
 	})
 	db.book.create({
 		title: "The World of Ice & Fire: The Untold History of Westeros and the Game of Thrones(A Song of Ice and Fire)",
 		author: "George R.R. Martin",
 		genre: ["Fantasy", "Epic Fantasy", "Fiction"],
 		pages: 326,
-		linkid: 17345242
+		linkid: 17345242,
+		series: true,
+		published: 2014,
+		language: "English"
 	})
 	db.book.create({
-		title: "Cinder",
+		title: "Cinder(The Lunar Chronicles #1)",
 		author: "Marissa Meyer",
 		genre: ["Fantasy", "Young Adult", "Science Fiction"],
 		pages: 395,
-		linkid: 11235712
+		linkid: 11235712,
+		series: true,
+		published: 2012,
+		language: "English"
 	})
 	db.book.create({
 		title: "Gone with the Wind",
 		author: "Margaret Mitchell",
 		genre: ["Classics", "Historical Fiction", "Fiction"],
 		pages: 1037,
-		linkid: 18405
+		linkid: 18405,
+		series: false,
+		published: 1999,
+		language: "English"
 	})
 	db.book.create({
 		title: "The Night Circus",
 		author: "Erin Morgenstern",
 		genre: ["Fantasy", "Romance", "Fiction"],
 		pages: 400,
-		linkid: 9361589
+		linkid: 9361589,
+		series: false,
+		published: 2011,
+		language: "English"
 	})
 	db.book.create({
 		title: "Summer, Fireworks, and My Corpse",
 		author: "Otsuichi",
 		genre: ["Horror", "Asian Literature", "Short Stories"],
 		pages: 350,
-		linkid: 7326853
+		linkid: 7326853,
+		series: false,
+		published: 2010,
+		language: "English"
 	})
 	db.book.create({
 		title: "The Casual Vacancy",
 		author: "J.K. Rowling",
 		genre: ["Fiction", "Contemporary", "Mystery"],
 		pages: 503,
-		linkid: 13497818
+		linkid: 13497818,
+		series: false,
+		published: 2012,
+		language: "English"
+	})
+	db.book.create({
+		title: "The Dream of the Red Chamber",
+		author: "Cao Xueqin",
+		genre: ["Classics", "Fiction", "Asian Literature"],
+		pages: 352,
+		linkid: 535739,
+		series: false,
+		published: 1958,
+		language: "Chinese"
 	})
 })
 
--- routes/search.js
+++ routes/search.js
@@ -13,70 +13,548 @@ router.get('/', (req, res) => {
 //Post
 
 router.post('/', (req, res) => {
-	// let inputText = req.body.inputText
-	let author = req.body.author
-	let genre = req.body.genre
-	let pagesMax = req.body.pagesMax
+	let publish = []
 
-	if(author != "") {
-		db.book.findAll({
-			where: {
-				author: author
-			}
-		}).then(book => {
-			let result = []
-			let newResult = []
-			for(let i = 0; i < book.length; i++) {
-				for(let j = 0; j < book[i].genre.length; j++) {
-					if(pagesMax >= book[i].pages && genre == book[i].genre[j]) {
-						result.push({
-							title: book[i].title,
-							author: book[i].author,
-							genre: book[i].genre,
-							pages: book[i].pages,
-							linkid: book[i].linkid
-						})
-					}
-				}
-				
-			}
-			return result
-		}).then(book => {
-			if(book != ""){
-				res.render('result', {book: book})
-			}
-			else {
-				res.send("No Pages! No genre!")
-			}
-		})
+	if(req.body.series == 0) {
+		req.body.series = undefined
+	}
+
+	if(req.body.language == 0) {
+		req.body.language = undefined
+	}
+
+	//Put publishing years into an array
+	if(req.body.twoTen == "true") {
+		publish.push(2010)
+	}
+	if(req.body.two == "true") {
+		publish.push(2000)
+	}
+	if(req.body.nineFive == "true") {
+		publish.push(1950)
+	}
+	if(req.body.nine == "true") {
+		publish.push(1900)
+	}
+	if(req.body.eigth == "true") {
+		publish.push(1800)
+	}
+
+	//Give series properties true/false
+	if(req.body.series == undefined) {
+		req.body.series = false
 	}
 	else {
-		db.book.findAll().then(book => {
-			let result = []
-			for(let i = 0; i < book.length; i++) {
-				for(let j = 0; j < book[i].genre.length; j++) {
-					if(pagesMax >= book[i].pages && genre == book[i].genre[j]) {
-						result.push({
-							title: book[i].title,
-							author: book[i].author,
-							genre: book[i].genre,
-							pages: book[i].pages,
-							linkid: book[i].linkid
-						})
-					}
-				}
-				
-			}
-			return result
-		}).then(book => {
-			if(book != ""){
-				res.render('result', {book: book})
-			}
-			else {
-				res.send("No Pages! No genre!")
-			}
-		})
+		req.body.series = true
 	}
+
+	//Filter options
+	// let filter = {
+	// 	title: req.body.inputText || undefined,
+	// 	author: req.body.author || undefined,
+	// 	genre: {
+	// 		$contains: [req.body.genre || undefined]
+	// 	},
+	// 	pages: {
+	// 		$between: [(req.body.pagesMin, req.body.pagesMax) || undefined]
+	// 	},
+	// 	series: req.body.series,
+	// 	published: {
+	// 		$between: [(publish.slice(-1)[0], new Date().getFullYear()) || (0, new Date().getFullYear())]
+	// 	},
+	// 	language: req.body.language || undefined
+	// }
+
+	let filter = {
+		title: req.body.inputText || '*',
+		author: req.body.author || '*',
+		genre: req.body.genre || '*',
+		pagesMin: req.body.pagesMin || 0,
+		pagesMax: req.body.pagesMax || 99999999999,
+		series: req.body.series,
+		published: publish.slice(-1)[0] || 1800,
+		language: req.body.language || '*'
+	}
+
+	console.log(filter)
+
+	// db.book.findAll({
+	// 	where: {
+	// 		filter
+	// 	}
+	// }).then(book => {
+	// 	console.log(book)
+	// })
+
+	db.book.findAll({
+		where: {
+			// author: filter.author,
+			// genre: {
+			// 	$contains: [filter.genre]
+			// },
+			pages: {
+				$between: [filter.pagesMin, filter.pagesMax]
+			},
+			series: filter.series,
+			published: {
+				$between: [filter.published, new Date().getFullYear()]
+			}
+		}
+	}).then(books=>{
+		res.send(books)
+	})
+
+	//Author filled in
+	// if(author != "") {
+	// 	db.book.findAll({
+	// 		where: {
+	// 			author: author
+	// 		}
+	// 	}).then(book => {
+	// 		let result = []
+	// 		if(pagesMax != ""){
+	// 			//Max pages and language filled in
+	// 			if(language != ""){
+	// 				//Max pages, language and genre filled in
+	// 				if(genre != ""){
+	// 					for(let i = 0; i < book.length; i++) {
+	// 						for(let j = 0; j < book[i].genre.length; j++) {
+	// 							if(pagesMax >= book[i].pages && pagesMin <= book[i].pages && language == book[i].language && genre == book[i].genre[j]) {
+	// 								result.push({
+	// 									title: book[i].title,
+	// 									author: book[i].author,
+	// 									genre: book[i].genre,
+	// 									pages: book[i].pages,
+	// 									linkid: book[i].linkid,
+	// 									series: book[i].series,
+	// 									published: book[i].published,
+	// 									language: book[i].language
+	// 								})
+	// 							}
+	// 						}
+	// 					}
+	// 				}
+	// 				//Max pages, language but no genre filled in
+	// 				else {
+	// 					for(let i = 0; i < book.length; i++) {
+	// 						if(pagesMax >= book[i].pages && pagesMin <= book[i].pages && language == book[i].language) {
+	// 							result.push({
+	// 								title: book[i].title,
+	// 								author: book[i].author,
+	// 								genre: book[i].genre,
+	// 								pages: book[i].pages,
+	// 								linkid: book[i].linkid,
+	// 								series: book[i].series,
+	// 								published: book[i].published,
+	// 								language: book[i].language
+	// 							})
+	// 						}
+	// 					}
+	// 				}
+	// 			}
+	// 			//No language filled in
+	// 			else {
+	// 				//Max pages and genre filled in
+	// 				if(genre != ""){
+	// 					for(let i = 0; i < book.length; i++) {
+	// 						for(let j = 0; j < book[i].genre.length; j++) {
+	// 							if(pagesMax >= book[i].pages && pagesMin <= book[i].pages && genre == book[i].genre[j]) {
+	// 								result.push({
+	// 									title: book[i].title,
+	// 									author: book[i].author,
+	// 									genre: book[i].genre,
+	// 									pages: book[i].pages,
+	// 									linkid: book[i].linkid,
+	// 									series: book[i].series,
+	// 									published: book[i].published,
+	// 									language: book[i].language
+	// 								})
+	// 							}
+	// 						}
+	// 					}
+	// 				}
+	// 				//Only max pages filled in
+	// 				else {
+	// 					for(let i = 0; i < book.length; i++) {
+	// 						if(pagesMax >= book[i].pages && pagesMin <= book[i].pages) {
+	// 							result.push({
+	// 								title: book[i].title,
+	// 								author: book[i].author,
+	// 								genre: book[i].genre,
+	// 								pages: book[i].pages,
+	// 								linkid: book[i].linkid,
+	// 								series: book[i].series,
+	// 								published: book[i].published,
+	// 								language: book[i].language
+	// 							})
+	// 						}
+	// 					}
+	// 				}
+	// 			}
+	// 		}
+	// 		//No max pages filled in
+	// 		else {
+	// 			//Language filled in
+	// 			if(language != ""){
+	// 				//Language and genre filled in
+	// 				if(genre != ""){
+	// 					for(let i = 0; i < book.length; i++) {
+	// 						for(let j = 0; j < book[i].genre.length; j++) {
+	// 							if(pagesMin <= book[i].pages && language == book[i].language && genre == book[i].genre[j]) {
+	// 								result.push({
+	// 									title: book[i].title,
+	// 									author: book[i].author,
+	// 									genre: book[i].genre,
+	// 									pages: book[i].pages,
+	// 									linkid: book[i].linkid,
+	// 									series: book[i].series,
+	// 									published: book[i].published,
+	// 									language: book[i].language
+	// 								})
+	// 							}
+	// 						}
+	// 					}
+	// 				}
+	// 				//Only language filled in
+	// 				else {
+	// 					for(let i = 0; i < book.length; i++) {
+	// 						if(pagesMin <= book[i].pages && language == book[i].language) {
+	// 							result.push({
+	// 								title: book[i].title,
+	// 								author: book[i].author,
+	// 								genre: book[i].genre,
+	// 								pages: book[i].pages,
+	// 								linkid: book[i].linkid,
+	// 								series: book[i].series,
+	// 								published: book[i].published,
+	// 								language: book[i].language
+	// 							})
+	// 						}
+	// 					}
+	// 				}
+	// 			}
+	// 			//Nothing filled in
+	// 			else {
+	// 				for(let i = 0; i < book.length; i++) {
+	// 					result.push({
+	// 						title: book[i].title,
+	// 						author: book[i].author,
+	// 						genre: book[i].genre,
+	// 						pages: book[i].pages,
+	// 						linkid: book[i].linkid,
+	// 						series: book[i].series,
+	// 						published: book[i].published,
+	// 						language: book[i].language
+	// 					})
+	// 				}
+	// 			}
+	// 		}
+	// 		return result
+	// 	}).then(book => {
+	// 		if(book != ""){
+	// 			res.render('result', {book: book})
+	// 		}
+	// 		else {
+	// 			res.send("Nothing filled in!")
+	// 		}
+	// 	})
+	// }
+	// else {
+	// 	//No author filled in
+	// 	db.book.findAll().then(book => {
+	// 		let result = []
+	// 		//Series = true
+	// 		if(series != "") {
+	// 			//Max pages filled in
+	// 			if(pagesMax != ""){
+	// 				//Max pages and language filled in
+	// 				if(language != ""){
+	// 					//Max pages, language and genre filled in
+	// 					if(genre != ""){
+	// 						for(let i = 0; i < book.length; i++) {
+	// 							for(let j = 0; j < book[i].genre.length; j++) {
+	// 								if(series == "true" && pagesMax >= book[i].pages && pagesMin <= book[i].pages && language == book[i].language && genre == book[i].genre[j]) {
+	// 									result.push({
+	// 										title: book[i].title,
+	// 										author: book[i].author,
+	// 										genre: book[i].genre,
+	// 										pages: book[i].pages,
+	// 										linkid: book[i].linkid,
+	// 										series: book[i].series,
+	// 										published: book[i].published,
+	// 										language: book[i].language
+	// 									})
+	// 								}
+	// 							}
+	// 						}
+	// 					}
+	// 					//Max pages, language but no genre filled in
+	// 					else {
+	// 						for(let i = 0; i < book.length; i++) {
+	// 							if(pagesMax >= book[i].pages && pagesMin <= book[i].pages && language == book[i].language) {
+	// 								result.push({
+	// 									title: book[i].title,
+	// 									author: book[i].author,
+	// 									genre: book[i].genre,
+	// 									pages: book[i].pages,
+	// 									linkid: book[i].linkid,
+	// 									series: book[i].series,
+	// 									published: book[i].published,
+	// 									language: book[i].language
+	// 								})
+	// 							}
+	// 						}
+	// 					}
+	// 				}
+	// 				//No language filled in
+	// 				else {
+	// 					//Max pages and genre filled in
+	// 					if(genre != ""){
+	// 						for(let i = 0; i < book.length; i++) {
+	// 							for(let j = 0; j < book[i].genre.length; j++) {
+	// 								if(pagesMax >= book[i].pages && pagesMin <= book[i].pages && genre == book[i].genre[j]) {
+	// 									result.push({
+	// 										title: book[i].title,
+	// 										author: book[i].author,
+	// 										genre: book[i].genre,
+	// 										pages: book[i].pages,
+	// 										linkid: book[i].linkid,
+	// 										series: book[i].series,
+	// 										published: book[i].published,
+	// 										language: book[i].language
+	// 									})
+	// 								}
+	// 							}
+	// 						}
+	// 					}
+	// 					//Only max pages filled in
+	// 					else {
+	// 						for(let i = 0; i < book.length; i++) {
+	// 							if(pagesMax >= book[i].pages && pagesMin <= book[i].pages) {
+	// 								result.push({
+	// 									title: book[i].title,
+	// 									author: book[i].author,
+	// 									genre: book[i].genre,
+	// 									pages: book[i].pages,
+	// 									linkid: book[i].linkid,
+	// 									series: book[i].series,
+	// 									published: book[i].published,
+	// 									language: book[i].language
+	// 								})
+	// 							}
+	// 						}
+	// 					}
+	// 				}
+	// 			}
+	// 			//No max pages filled in
+	// 			else {
+	// 				//Language filled in
+	// 				if(language != ""){
+	// 					//Language and genre filled in
+	// 					if(genre != ""){
+	// 						for(let i = 0; i < book.length; i++) {
+	// 							for(let j = 0; j < book[i].genre.length; j++) {
+	// 								if(pagesMin <= book[i].pages && language == book[i].language && genre == book[i].genre[j]) {
+	// 									result.push({
+	// 										title: book[i].title,
+	// 										author: book[i].author,
+	// 										genre: book[i].genre,
+	// 										pages: book[i].pages,
+	// 										linkid: book[i].linkid,
+	// 										series: book[i].series,
+	// 										published: book[i].published,
+	// 										language: book[i].language
+	// 									})
+	// 								}
+	// 							}
+	// 						}
+	// 					}
+	// 					//Only language filled in
+	// 					else {
+	// 						for(let i = 0; i < book.length; i++) {
+	// 							if(pagesMin <= book[i].pages && language == book[i].language) {
+	// 								result.push({
+	// 									title: book[i].title,
+	// 									author: book[i].author,
+	// 									genre: book[i].genre,
+	// 									pages: book[i].pages,
+	// 									linkid: book[i].linkid,
+	// 									series: book[i].series,
+	// 									published: book[i].published,
+	// 									language: book[i].language
+	// 								})
+	// 							}
+	// 						}
+	// 					}
+	// 				}
+	// 				//Nothing filled in
+	// 				else {
+	// 					for(let i = 0; i < book.length; i++) {
+	// 						result.push({
+	// 							title: book[i].title,
+	// 							author: book[i].author,
+	// 							genre: book[i].genre,
+	// 							pages: book[i].pages,
+	// 							linkid: book[i].linkid,
+	// 							series: book[i].series,
+	// 							published: book[i].published,
+	// 							language: book[i].language
+	// 						})
+	// 					}
+	// 				}
+	// 			}
+	// 		}
+	// 		//Series = false
+	// 		else {
+	// 			//Max pages filled in
+	// 			if(pagesMax != ""){
+	// 				//Max pages and language filled in
+	// 				if(language != ""){
+	// 					//Max pages, language and genre filled in
+	// 					if(genre != ""){
+	// 						for(let i = 0; i < book.length; i++) {
+	// 							for(let j = 0; j < book[i].genre.length; j++) {
+	// 								if(series == "false" && pagesMax >= book[i].pages && pagesMin <= book[i].pages && language == book[i].language && genre == book[i].genre[j]) {
+	// 									result.push({
+	// 										title: book[i].title,
+	// 										author: book[i].author,
+	// 										genre: book[i].genre,
+	// 										pages: book[i].pages,
+	// 										linkid: book[i].linkid,
+	// 										series: book[i].series,
+	// 										published: book[i].published,
+	// 										language: book[i].language
+	// 									})
+	// 								}
+	// 							}
+	// 						}
+	// 					}
+	// 					//Max pages, language but no genre filled in
+	// 					else {
+	// 						for(let i = 0; i < book.length; i++) {
+	// 							if(pagesMax >= book[i].pages && pagesMin <= book[i].pages && language == book[i].language) {
+	// 								result.push({
+	// 									title: book[i].title,
+	// 									author: book[i].author,
+	// 									genre: book[i].genre,
+	// 									pages: book[i].pages,
+	// 									linkid: book[i].linkid,
+	// 									series: book[i].series,
+	// 									published: book[i].published,
+	// 									language: book[i].language
+	// 								})
+	// 							}
+	// 						}
+	// 					}
+	// 				}
+	// 				//No language filled in
+	// 				else {
+	// 					//Max pages and genre filled in
+	// 					if(genre != ""){
+	// 						for(let i = 0; i < book.length; i++) {
+	// 							for(let j = 0; j < book[i].genre.length; j++) {
+	// 								if(pagesMax >= book[i].pages && pagesMin <= book[i].pages && genre == book[i].genre[j]) {
+	// 									result.push({
+	// 										title: book[i].title,
+	// 										author: book[i].author,
+	// 										genre: book[i].genre,
+	// 										pages: book[i].pages,
+	// 										linkid: book[i].linkid,
+	// 										series: book[i].series,
+	// 										published: book[i].published,
+	// 										language: book[i].language
+	// 									})
+	// 								}
+	// 							}
+	// 						}
+	// 					}
+	// 					//Only max pages filled in
+	// 					else {
+	// 						for(let i = 0; i < book.length; i++) {
+	// 							if(pagesMax >= book[i].pages && pagesMin <= book[i].pages) {
+	// 								result.push({
+	// 									title: book[i].title,
+	// 									author: book[i].author,
+	// 									genre: book[i].genre,
+	// 									pages: book[i].pages,
+	// 									linkid: book[i].linkid,
+	// 									series: book[i].series,
+	// 									published: book[i].published,
+	// 									language: book[i].language
+	// 								})
+	// 							}
+	// 						}
+	// 					}
+	// 				}
+	// 			}
+	// 			//No max pages filled in
+	// 			else {
+	// 				//Language filled in
+	// 				if(language != ""){
+	// 					//Language and genre filled in
+	// 					if(genre != ""){
+	// 						for(let i = 0; i < book.length; i++) {
+	// 							for(let j = 0; j < book[i].genre.length; j++) {
+	// 								if(pagesMin <= book[i].pages && language == book[i].language && genre == book[i].genre[j]) {
+	// 									result.push({
+	// 										title: book[i].title,
+	// 										author: book[i].author,
+	// 										genre: book[i].genre,
+	// 										pages: book[i].pages,
+	// 										linkid: book[i].linkid,
+	// 										series: book[i].series,
+	// 										published: book[i].published,
+	// 										language: book[i].language
+	// 									})
+	// 								}
+	// 							}
+	// 						}
+	// 					}
+	// 					//Only language filled in
+	// 					else {
+	// 						for(let i = 0; i < book.length; i++) {
+	// 							if(pagesMin <= book[i].pages && language == book[i].language) {
+	// 								result.push({
+	// 									title: book[i].title,
+	// 									author: book[i].author,
+	// 									genre: book[i].genre,
+	// 									pages: book[i].pages,
+	// 									linkid: book[i].linkid,
+	// 									series: book[i].series,
+	// 									published: book[i].published,
+	// 									language: book[i].language
+	// 								})
+	// 							}
+	// 						}
+	// 					}
+	// 				}
+	// 				//Nothing filled in
+	// 				else {
+	// 					for(let i = 0; i < book.length; i++) {
+	// 						result.push({
+	// 							title: book[i].title,
+	// 							author: book[i].author,
+	// 							genre: book[i].genre,
+	// 							pages: book[i].pages,
+	// 							linkid: book[i].linkid,
+	// 							series: book[i].series,
+	// 							published: book[i].published,
+	// 							language: book[i].language
+	// 						})
+	// 					}
+	// 				}
+	// 			}
+	// 		}
+	// 		return result
+	// 	}).then(book => {
+	// 		if(book != ""){
+	// 			res.render('result', {book: book})
+	// 		}
+	// 		else {
+	// 			res.send("Nothing filled in!")
+	// 		}
+	// 	})
+	// }
 })
 
 module.exports = router
