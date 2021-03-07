134b7c2ea9f52051cdbdf356def2e9e42e528e62
Arun Pandey <apandey@ex2india.com>
2021-03-07 12:12:01 +0530

    Added BookDreamer File

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
-# Book Recommendation App - Find Your Next Read
+# BookDreamer - Find Your Next Read
+
 This app is for all the people that do not know what book they want to read this time.
+
+This app is made as a final year project
 
 In this app you can search for a book using a variety of filter options, then in the results you can sort either on title, author, page number or rating.
 The information on the books are from the goodreads site (goodreads.com).
@@ -9,6 +12,7 @@ There are two different databases to search through, a self-made small, but comp
 The app is still a work in progress. What I also want to implement is the ability for users of Goodreads to login and search through their own to-read list, in order to make the results even more specific.
 
 The app is made with the following tools and frameworks:
+
 - Node.js (Express)
 - JavaScript
 - jQuery
--- app.js
+++ app.js
@@ -1,30 +1,29 @@
 // Set up libraries
-const sequelize 	= require('sequelize')
-const express 		= require('express')
-const bodyParser 	= require('body-parser')
-const session 		= require('express-session')
-const pug			= require('pug')
-const pg 			= require('pg')
-const sass			= require('node-sass')
-const db			= require(__dirname + '/routes/database.js')
+const sequelize = require('sequelize');
+const express = require('express');
+const bodyParser = require('body-parser');
+const session = require('express-session');
+const pug = require('pug');
 
-const app			= express()
+const db = require(__dirname + '/routes/database.js');
 
-app.set( 'view engine', 'pug')
-app.set( 'views', __dirname + '/views' )
+const app = express();
 
-// Routes
-let searchRoute 		= require(__dirname + '/routes/search')
-let findJsonRoute		= require(__dirname + '/routes/findJson')
-
-app.use( express.static('static'))
-app.use( bodyParser.urlencoded({extended: true}))
+app.set('view engine', 'pug');
+app.set('views', __dirname + '/views');
 
 // Routes
-app.use( searchRoute )
-app.use( findJsonRoute )
+const searchRoute = require(__dirname + '/routes/search');
+const findJsonRoute = require(__dirname + '/routes/findJson');
+
+app.use(express.static('static'));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Routes
+app.use(searchRoute);
+app.use(findJsonRoute);
 
 //listen port 8000
 app.listen(8000, () => {
-	console.log('Server is running')
-})
+  console.log('Server is running');
+});
